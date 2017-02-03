e8b69eb3e94f6b6f8d4727bc6eb311629554864f
bruncun <bruncun@icloud.com>
2017-02-03 17:17:06 -0500

    Replace manual turbo links asset with bower asset

--- _assets/javascripts/vendor.js
+++ _assets/javascripts/vendor.js
@@ -2,5 +2,5 @@
 //= require vendor/jquery-throttle-debounce.js
 //= require fluidbox
 //= require scrollreveal
+//= require turbolinks5/dist/turbolinks.js
 //= require vendor/retina.js
-//= require vendor/turbolinks.js
