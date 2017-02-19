542ecc7c244b873b6554e8031e37dc5892e63bba
bruncun <bruno@brunocunha.net>
2017-02-19 15:21:16 -0500

    Fix ga snippet (#50)
    * Update JS asset load order
    
    * Update google analytics snippet

--- _assets/javascripts/application.js
+++ _assets/javascripts/application.js
@@ -1,13 +1,12 @@
 //= require_self
 
-$(document).on('turbolinks:load', function (event) {
+$(document).on('turbolinks:load', function () {
   // Initialize fluidbox
   $('.fluidbox-trigger').fluidbox();
 
   // Track page views on Turbolinks
   if (typeof ga === 'function') {
-    ga('set', 'location', event.data.url)
-    ga('send', 'pageview')
+    ga('send', 'pageview', location.pathname);
   }
 
   // Initialize scrollreveal
