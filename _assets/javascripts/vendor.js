375cecf86084feb69b0fb6ff6193911489f577f7
Nielsen Ramon <hello@nielsenramon.com>
2017-03-02 14:31:33 +0100

    gh-pages | Fix social media links in navigation [ci skip]

--- _assets/javascripts/vendor.js
+++
@@ -1,6 +0,0 @@
-//= require jquery
-//= require vendor/jquery-throttle-debounce.js
-//= require fluidbox
-//= require scrollreveal
-//= require turbolinks5/dist/turbolinks.js
-//= require vendor/retina.js
---
+++ assets/application.js
@@ -0,0 +1,23 @@
+
+$(document).on('turbolinks:load', function () {
+  // Initialize fluidbox
+  $('.fluidbox-trigger').fluidbox();
+
+  // Track page views on Turbolinks
+  if (typeof ga === 'function') {
+    ga('send', 'pageview', location.pathname);
+  }
+
+  // Initialize scrollreveal
+  sr.reveal('.reveal', {
+    distance: '0',
+    duration: 500,
+    easing: 'ease-in-out',
+    origin: 'top',
+    scale: 1,
+    reset: false,
+    viewFactor: 0
+  });
+});
+
+window.sr = ScrollReveal({ reset: true });
