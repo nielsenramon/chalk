6f91b3bcbe07b5a3b45de6309d72e6481442c193
Nielsen Ramon <hello@nielsenramon.com>
2017-06-06 09:59:10 +0200

    Fix htmlproofer warnings

--- _assets/javascripts/vendor.js
+++ _assets/javascripts/vendor.js
@@ -1,6 +1,6 @@
 //= require jquery
 //= require throttle-debounce-fn/dist/throttle-debounce-fn.js
-//= require fluidbox
+//= require fluidbox/dist/js/jquery.fluidbox.min.js
 //= require scrollreveal
 //= require turbolinks/dist/turbolinks.js
-//= require retinajs
+//= require retinajs/dist/retina.min.js
