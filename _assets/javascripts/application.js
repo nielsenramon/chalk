8f7230b6b5c2f9ea30e1277d56047b42280b1ac4
Miguel de Moura <me@migueldemoura.com>
2017-06-12 01:45:46 +0100

    Fix appear on scroll - #63

--- _assets/javascripts/application.js
+++ _assets/javascripts/application.js
@@ -7,6 +7,11 @@ $('.btn-share').on('click', function() {
 });
 
 $(document).on('turbolinks:load', function () {
+  // Initialize ScrollAppear
+  if (typeof $.fn.scrollAppear === 'function') {
+    $('.appear').scrollAppear();
+  }
+
   // Initialize fluidbox
   $('.fluidbox-trigger').fluidbox();
 
@@ -14,17 +19,4 @@ $(document).on('turbolinks:load', function () {
   if (typeof ga === 'function') {
     ga('send', 'pageview', location.pathname);
   }
-
-  // Initialize scrollreveal
-  sr.reveal('.reveal', {
-    distance: '0',
-    duration: 500,
-    easing: 'ease-in-out',
-    origin: 'top',
-    scale: 1,
-    reset: false,
-    viewFactor: 0
-  });
 });
-
-window.sr = ScrollReveal({ reset: true });
--- _assets/javascripts/vendor.js
+++ _assets/javascripts/vendor.js
@@ -1,6 +1,5 @@
 //= require jquery
 //= require throttle-debounce-fn/dist/throttle-debounce-fn.js
 //= require fluidbox
-//= require scrollreveal
 //= require turbolinks/dist/turbolinks.js
 //= require retinajs
